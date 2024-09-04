751e74b349cffa6a0927bb4b60b76340a5848c5c
matheusmviana@outlook.com.br <matheusmviana@outlook.com.br>
2024-09-03 22:11:42 -0300

    feat: jokes page (wip)

--- src/pages/Jokes/index.jsx
+++ src/pages/Jokes/index.jsx
@@ -1,9 +1,51 @@
-import './styles.css'
+import './styles.css';
+import { useState, useRef } from 'react';
 
 export default function Jokes() {
+    const [joke, setJoke] = useState()
+
+    const [dropdown, setDropdown] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdown(!dropdown);
+    };
+
+    const categories = [
+        "animal", "career", "celebrity", "dev", "explicit", "fashion",
+        "food", "history", "money", "movie", "music", "political",
+        "religion", "science", "sport", "travel"
+    ];
+
+    const getJokeByCategory = (async (e) => {
+        try {
+            const response = await fetch(`https://api.chucknorris.io/jokes/random?category=${e.target.value}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+
+            const result = await response.json()
+
+            setJoke(result)
+        } catch (error) {
+
+        }
+    })
+
     return (
-        <>
-            <h1>jokes</h1>
-        </>
-    )
+        <section>
+            <h2>Select a category to get a random joke</h2>
+            <div className="dropdown">
+                <button onClick={toggleDropdown} className="navLink">
+                    Categories
+                </button>
+                <ul className={`dropdownLinks ${dropdown ? "activeDropdown" : "inactiveDropdown"}`}>
+                    {categories.map((category, index) => (
+                        <li key={index}>{category}</li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    );
 }
