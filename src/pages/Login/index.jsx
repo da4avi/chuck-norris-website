16cba4f6020a445612cb795aba41eeb4eafef46c
davi <finkler.davi41@gmail.com>
2024-09-03 22:02:06 -0300

    feat: login and fix: home

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,27 @@
+import { Link } from 'react-router-dom'
 import './styles.css'
 
 export default function Login () {
     return (
         <>
         <h1>Login</h1>
+        <form action="">
+            <div>
+                <label htmlFor="">User: </label>
+                <input type="text" name="" id="" />
+            </div>
+            <div>
+                <label htmlFor="">Email: </label>
+                <input type="email" name="" id="" />
+            </div>
+            <div>
+                <label htmlFor="">Password: </label>
+                <input type="password" name="" id="" />
+            </div>
+        </form>
+        <li>
+            <Link to="/">Sign In</Link>
+        </li>
         </>
     )
 }
