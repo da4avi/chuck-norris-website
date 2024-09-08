4d2806b934ec516243d062b50af5026b4f92bff9
davi <finkler.davi41@gmail.com>
2024-09-08 19:17:35 -0300

    feat: css in home page and about chuck norris

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,9 +1,33 @@
+import { Link } from 'react-router-dom'
 import './styles.css'
 
 export default function Register () {
     return (
         <>
-        <h1>register</h1>
+        <div>
+        <h1>Sign in</h1>
+        <form action="">
+            <div>
+                <label htmlFor="">User: </label>
+                <input type="text" name="" id="" />
+            </div>
+            <div>
+                <label htmlFor="">Email: </label>
+                <input type="email" name="" id="" />
+            </div>
+            <div>
+                <label htmlFor="">Password: </label>
+                <input type="password" name="" id="" />
+            </div>
+            <div>
+                <label htmlFor="">Confirm your password: </label>
+                <input type="password" name="" id="" />
+            </div>
+        </form>
+        <li>
+            <Link to="/login">Login </Link>
+        </li>
+        </div>
         </>
     )
 }
