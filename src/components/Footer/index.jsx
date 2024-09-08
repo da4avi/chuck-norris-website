ddc9069fb64f2ae630ba4f712f4e0b3f1253cfa1
matheusmviana@outlook.com.br <matheusmviana@outlook.com.br>
2024-09-08 18:11:42 -0300

    feat: footer (initial structute)

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom"
 import "./styles.css"
 
-export default function Footer () {
+export default function Footer() {
     return (
-        <>
-        <h1>Footer</h1>
-        </>
+        <footer className="main-footer">
+            <div className="site-map">
+                <h2>Website Map</h2>
+                <ul>
+                    <li><Link to="/home">Home</Link></li>
+                    <li><Link to="/jokes">Jokes</Link></li>
+                    <li><Link to="/aboutchucknorris">About the Chuck Norris</Link></li>
+                    <li><Link to="/aboutthecreators">About the creators</Link></li>
+                    <li><Link to="/register">Sign In</Link></li>
+                </ul>
+            </div>
+        </footer>
     )
 }
