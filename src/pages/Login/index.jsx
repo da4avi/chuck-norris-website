d1ac45e13416160ceefd0a065cff0ed03e9591f6
davi <finkler.davi41@gmail.com>
2024-09-10 20:38:13 -0300

    feat: style login and register

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,27 +1,37 @@
 import { Link } from 'react-router-dom'
 import './styles.css'
 
-export default function Login () {
+export default function Login() {
     return (
-        <>
-        <h1>Login</h1>
-        <form action="">
-            <div>
-                <label htmlFor="">User: </label>
-                <input type="text" name="" id="" />
+        <div className='login'>
+            <h1>Login</h1>
+            <div className='card'>
+                <form action="">
+                    <div>
+                        <label htmlFor="">User: </label>
+                        <br />
+                        <input type="text" name="" id="" />
+                    </div>
+                    <div>
+                        <label htmlFor="">Email: </label>
+                        <br />
+                        <input type="email" name="" id="" />
+                    </div>
+                    <div>
+                        <label htmlFor="">Password: </label>
+                        <br />
+                        <input type="password" name="" id="" />
+                    </div>
+                </form>
+                <li>
+                    <Link to="/">
+                        <button type="button">Login</button>
+                    </Link>
+                    <Link to="/register">
+                        <button type="button">Sign in</button>
+                    </Link>
+                </li>
             </div>
-            <div>
-                <label htmlFor="">Email: </label>
-                <input type="email" name="" id="" />
-            </div>
-            <div>
-                <label htmlFor="">Password: </label>
-                <input type="password" name="" id="" />
-            </div>
-        </form>
-        <li>
-            <Link to="/register">Sign In</Link>
-        </li>
-        </>
+        </div>
     )
 }
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -3,31 +3,40 @@ import './styles.css'
 
 export default function Register () {
     return (
-        <>
-        <div>
-        <h1>Sign in</h1>
-        <form action="">
-            <div>
-                <label htmlFor="">User: </label>
-                <input type="text" name="" id="" />
+        <div className='register'>
+            <h1>Sign in</h1>
+            <div className='card'>
+                <form action="">
+                    <div>
+                        <label htmlFor="">User: </label>
+                        <br />
+                        <input type="text" name="" id="" />
+                    </div>
+                    <div>
+                        <label htmlFor="">Email: </label>
+                        <br />
+                        <input type="email" name="" id="" />
+                    </div>
+                    <div>
+                        <label htmlFor="">Password: </label>
+                        <br />
+                        <input type="password" name="" id="" />
+                    </div>
+                    <div>
+                        <label htmlFor="">Confirm Password: </label>
+                        <br />
+                        <input type="password" name="" id="" />
+                    </div>
+                </form>
+                <li>
+                    <Link to="/login">
+                        <button type="button">Create</button>
+                    </Link>
+                    <Link to="/login">
+                        <button type="button">Login</button>
+                    </Link>
+                </li>
             </div>
-            <div>
-                <label htmlFor="">Email: </label>
-                <input type="email" name="" id="" />
-            </div>
-            <div>
-                <label htmlFor="">Password: </label>
-                <input type="password" name="" id="" />
-            </div>
-            <div>
-                <label htmlFor="">Confirm your password: </label>
-                <input type="password" name="" id="" />
-            </div>
-        </form>
-        <li>
-            <Link to="/login">Login </Link>
-        </li>
         </div>
-        </>
     )
 }
