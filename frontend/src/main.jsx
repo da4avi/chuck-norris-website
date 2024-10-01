aa973cac26630dffd0f049525ece718e37a83bd0
davi <finkler.davi41@gmail.com>
2024-09-30 22:03:00 -0300

    fix: starting to use i18next to translation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,26 @@ import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next'
+
+import global_en from './components/Locales/en/global.json'
+import global_pt from './components/Locales/pt/global.json'
+
+i18next.init({
+    interpolation: { escapeValue: false },
+    lng: "pt",
+    en: {
+        global_en
+    },
+    pt: {
+        global_pt
+    }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <I18nextProvider i18n={i18next}>
+        <App />
+    </I18nextProvider>
 )
 
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -2,8 +2,17 @@ import { Link } from 'react-router-dom'
 import './styles.css'
 import { useEffect } from 'react';
 import { useState } from 'react'
+import translate from "translate";
+translate.engine = "google";
+import { useTranslation } from 'react-i18next'
 
 export default function Home() {
+    const [t, i18n] = useTranslation("global.json")
+
+    const changeLang = (lang) => {
+        i18n.changeLanguage(lang)
+    }
+
     const [joke, setJoke] = useState(<>Loading...</>)
 
     async function getRandomJoke() {
@@ -20,24 +29,28 @@ export default function Home() {
 
         const data = await response.json();
 
-        console.log(response)
+        const translated = await translate(data.value, "pt");
 
-        return setJoke(data.value)
+        return setJoke(translated)
     }
 
     useEffect(() => {
         if (!joke || joke != "Loading...") {
             getRandomJoke()
         }
+        console.log(i18n)
     }, [])
 
+    
     return (
 
         <div className='home' >
             <section className='texto'>
                 <h2>Who was</h2>
                 <br />
-                <p className='p'>Chuck Norris is a martial artist, actor, and cultural icon, famous for his tough-guy image and legendary roundhouse kick. Rising to fame in the 1970s with movies like Way of the Dragon, he later became a TV star in Walker, Texas Ranger. Beyond acting, Norris has founded martial arts organizations and programs like Kickstart Kids, impacting thousands of lives. He’s also a pop culture legend thanks to the humorous "Chuck Norris Facts."</p>
+                <button onClick={ () => changeLang("en") } className='m-2'>en</button>
+                <button onClick={ () => changeLang("pt") } className='m-2'>pt</button>
+                <p className='p'>{t("home")}</p>
                 <br />
                 <li><Link to="/aboutchucknorris">Read More</Link></li>
             </section>
