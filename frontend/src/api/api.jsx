71c71eb921e2424e7b892221ba705b1843a1b251
matheusmviana@outlook.com.br <matheusmviana@outlook.com.br>
2024-11-05 21:24:04 -0300

    feat: adding .env variables

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,24 +1,22 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
-    timeout: 10000
-})
-
-//funçao para pegar o token do storage e enviar em cada uma das requisiçoes
+  baseURL: import.meta.env.VITE_PROD_BACKEND_URL || "http://localhost:8000",
+  timeout: 10000,
+});
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token')
-        if (token) {
-            config.headers.Authorization = token
-        }
-
-        return config
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = token;
     }
-)
 
-export default api
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
